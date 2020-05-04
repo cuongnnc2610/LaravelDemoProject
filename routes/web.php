710038a7775fce53c1772df4dbbcfd465a227d33
Nguyen Ngoc Cuong <cuongnn@domain.vnext.vn>
2020-05-04 17:41:11 +0700

    interface

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,41 @@ use App\TheLoai;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function(){
+	Route::group(['prefix' => 'theloai'], function(){
+		Route::get('danhsach', 'TheLoaiController@get');
+		Route::get('sua', 'TheLoaiController@edit');
+		Route::get('them', 'TheLoaiController@add');
+	});
+
+	Route::group(['prefix' => 'loaitin'], function(){
+		Route::get('danhsach', 'LoaiTinController@get');
+		Route::get('sua', 'LoaiTinController@edit');
+		Route::get('them', 'LoaiTinController@add');
+	});
+
+	Route::group(['prefix' => 'tintuc'], function(){
+		Route::get('danhsach', 'TinTucController@get');
+		Route::get('sua', 'TinTucController@edit');
+		Route::get('them', 'TinTucController@add');
+	});
+
+	Route::group(['prefix' => 'slide'], function(){
+		Route::get('danhsach', 'SlideController@get');
+		Route::get('sua', 'SlideController@edit');
+		Route::get('them', 'SlideController@add');
+	});
+
+	Route::group(['prefix' => 'user'], function(){
+		Route::get('danhsach', 'UserController@get');
+		Route::get('sua', 'UserController@edit');
+		Route::get('them', 'UserController@add');
+	});
+
+	Route::group(['prefix' => 'comment'], function(){
+		Route::get('danhsach', 'CommentController@get');
+		Route::get('sua', 'CommentController@edit');
+		Route::get('them', 'CommentController@add');
+	});
+});
