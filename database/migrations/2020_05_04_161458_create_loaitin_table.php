9937e749ec70168e542b9324b53e4ed76588d3fa
Nguyen Ngoc Cuong <cuongnn@domain.vnext.vn>
2020-05-04 17:11:39 +0700

    create database

--- database/migrations/2020_05_04_161458_create_loaitin_table.php
+++ database/migrations/2020_05_04_161458_create_loaitin_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateLoaitinTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('loaitin', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email', 191)->unique();
-            $table->integer('level');
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('idTheLoai')->unsigned();
+            $table->foreign('idTheLoai')->references('id')->on('TheLoai');
+            $table->string('Ten');
+            $table->string('TenKhongDau');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('loaitin');
     }
 }
